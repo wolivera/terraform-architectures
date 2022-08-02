1bbd10f037b466dbddd86014d3ffe24697a4b3b4
Will Olivera <wolivera@zircon.tech>
2022-08-02 10:29:02 -0300

    wip: aws beanstalk scripts

--- aws-fargate/terraform/variables.tf
+++ aws-fargate/terraform/variables.tf
@@ -34,7 +34,7 @@ variable "application_secrets" {
 
 variable "availability_zones" {
   description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "cidr" {
@@ -44,12 +44,12 @@ variable "cidr" {
 
 variable "private_subnets" {
   description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
+  default     = ["10.0.0.0/20", "10.0.32.0/20"]
 }
 
 variable "public_subnets" {
   description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
+  default     = ["10.0.16.0/20", "10.0.48.0/20"]
 }
 
 variable "service_desired_count" {
